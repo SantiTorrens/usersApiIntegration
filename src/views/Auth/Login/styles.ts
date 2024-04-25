c9650e7c53ee7333501e0e2fe5c50ce091dea020
Santiago <santiago@dempathy.com>
2024-04-25 11:51:51 +0200

    fix. login styles on mobile

--- src/views/Auth/Login/styles.ts
+++ src/views/Auth/Login/styles.ts
@@ -2,50 +2,59 @@ import styled from "styled-components";
 import MainLayout from "../../../layouts/MainLayout";
 
 // Styled component for the login page container
-export const LoginPageContainer = styled(MainLayout)`
-  .animatedHeadline {
-    /* Add your styles for the headline */
-  }
-`;
+export const LoginPageContainer = styled(MainLayout)``;
 
 // Styled component for the login form container
 export const LoginFormContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    @media (max-width: 1024px) {
+        flex-direction: column;
+    }
 `;
 export const HeaderContainer = styled.div`
-  width: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+export const Headline = styled.h2`
+    font-size: 150px;
+    line-height: 240px;
+    color: transparent;
+    -webkit-text-stroke: 2px white;
+    text-align: center;
+    @media (max-width: 1024px) {
+      font-size: 90px;
+    }
 
-`
+`;
 // Styled component for the form container
 export const FormContainer = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-left: auto;
-  margin-right: auto;
-  gap: 0.5rem;
-  width: 100%;
-  width: 80%;;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-left: auto;
+    margin-right: auto;
+    gap: 0.5rem;
+    width: 100%;
+    width: 80%;
 `;
 
 // Styled component for the button
 export const SubmitButton = styled.button`
-  width: 100%;
-  margin-top: 20px;
+    width: 100%;
+    margin-top: 20px;
 `;
 
 // Styled component for the horizontal rule
 export const HorizontalRule = styled.hr`
-  margin: 30px auto;
-  width: 100%;
-  max-width: 200px;
-  border: 0.5px solid black;
+    margin: 30px auto;
+    width: 100%;
+    max-width: 200px;
+    border: 0.5px solid black;
 `;
